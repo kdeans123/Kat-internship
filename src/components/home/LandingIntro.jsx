6cbb8056f3ea20408202084d7ab5ace5505fedfa
Kat <kat.m.deans@gmail.com>
2025-02-07 14:30:22 -0500

    Initialization of animations

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const LandingIntro = () => {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+
   return (
     <section id="section-intro" className="no-top no-bottom">
       <div className="container">
